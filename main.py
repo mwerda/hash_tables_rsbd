e005bb59cebd08d155538281dfb031d83361e9a6
mwerda <zmalqpzmalqp@gmail.com>
2018-01-23 01:58:44 +0100

    getting closer to iter 1

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 from prettytable import PrettyTable
 
+def get_size(relations_list, relation, variable):
+    return relations_list[relation]['SIZE', variable]
+
+def get_
+
 class Relation:
-    def __init__(self, number, card):
+    def __init__(self, id, cardinal):
         self.relation_table_schema = {
             'rows_identifiers': ['SIZE', 'VAL'],
             'columns_identifiers': ['Property', 'A', 'B']
         }
-        self.id = number
+        self.id = id
         self.table = {}
-        self.card = card
-        self.table = HashTable(self.relation_table_schema['rows_identifiers'],
+        self.cardinal = cardinal
+        self.hash_table = HashTable(self.relation_table_schema['rows_identifiers'],
                                self.relation_table_schema['columns_identifiers'])
 
     def populate_table(self, data):
-        self.table.populate(data)
+        self.hash_table.populate(data)
 
     # def build_table(self, columns, rows):
     #     self.table = PrettyTable(columns)
     #     for row in rows:
     #         self.table.add_row(row)
 
-    def __repr__(self):
-        print('R' + str(self.id))
-        self.table.__repr__()
-        print()
+    def __getitem__(self, item):
+        row, col = item
+        return self.hash_table.table[row][col]
+
+    def __setitem__(self, key, value):
+        self.hash_table.table[key] = value
+
+    def __str__(self):
+        return str(self.id + '\n' + self.hash_table.__str__() + '\n')
 
     #def get(self, row, column):
 
@@ -42,35 +52,43 @@ class HashTable:
             for value, column_name in zip(values_list, self.column_identifiers[1:]):
                 self.table[row_name][column_name] = value
 
-    def __repr__(self):
+    def __str__(self):
         pretty_table = PrettyTable(self.column_identifiers)
         for row_name in self.row_identifiers:
             row = [row_name]
             for column_name in self.column_identifiers[1:]:
                 row.append(self.table[row_name][column_name])
             pretty_table.add_row(row)
-        print(pretty_table)
+        return str(pretty_table)
 
 
-R1 = Relation(1, 1000)
+
+R1 = Relation('R1', 1000)
 R1.populate_table([[4, 4], [200, 50]])
 #R1.build_table(['Property', 'A', 'B'], [['SIZE', 4, 4], ['VAL', 200, 50]])
-R1.__repr__()
+# R1.__repr__()
 
-R2 = Relation(2, 1000)
+R2 = Relation('R2', 1000)
 R2.populate_table([[4, 4], [40, 100]])
 #R2.build_table(['Property', 'A', 'B'], [['SIZE', 4, 4], ['VAL', 40, 100]])
-R2.__repr__()
+# R2.__repr__()
 
-R3 = Relation(3, 2000)
+R3 = Relation('R3', 2000)
 R3.populate_table([[4, 4], [400, 100]])
 #R3.build_table(['Property', 'A', 'B'], [['SIZE', 4, 4], ['VAL', 400, 100]])
-R3.__repr__()
+# R3.__repr__()
 
-R4 = Relation(4, 1000)
+R4 = Relation('R4', 1000)
 R4.populate_table([[4, 4], [200, 50]])
 #R4.build_table(['Property', 'A', 'B'], [['SIZE', 4, 4], ['VAL', 200, 50]])
-R4.__repr__()
+# R4.__repr__()
+
+relations = {
+    'R1': R1,
+    'R2': R2,
+    'R3': R3,
+    'R4': R4
+}
 
 in_domains = {
     'A': 400,
@@ -78,6 +96,38 @@ in_domains = {
 }
 
 in_attributes = ['R1.A', 'R2.A', 'R2.B', 'R3.B', 'R4.B']
+factors = HashTable(in_attributes, ['Atrybut', 'SF'])
+
+factors_to_populate = []
+for attribute in in_attributes:
+    rel_id = attribute.split('.')[0]
+    rel_field = attribute.split('.')[1]
+    factors_to_populate.append([relations[rel_id]['VAL', rel_field] / in_domains[rel_field]])
+
+    relations[rel_id][attribute, 'SF'] = relations[rel_id]['VAL', rel_field] / in_domains[rel_field]
+factors.populate(factors_to_populate)
+print(factors)
+
+in_semi_joins_codes = ['R1 A R2',
+                 'R2 A R1',
+                 'R2 B R3',
+                 'R3 B R2',
+                 'R2 B R4',
+                 'R4 B R2',
+                 'R3 B R4',
+                 'R4 B R3']
+
+semi_joins = HashTable(in_semi_joins_codes, ['Join', 'KOSZT', 'EFEKT', 'ZYSK'])
+semi_joins_rows = []
+for element in in_semi_joins_codes:
+    rel_left = element.split(' ')[0]
+    rel_right = element.split(' ')[2]
+    variable = element.split(' ')[1]
+
+    koszt = get_size(relations, rel_right, variable) * relations[rel_right].cardinal
+    efekt
+
+
 
 #######################
 
@@ -97,11 +147,7 @@ in_attributes = ['R1.A', 'R2.A', 'R2.B', 'R3.B', 'R4.B']
 
 
 
-# factors_table = PrettyTable(['Atrybut', 'SF'])
-# PrettyTable()
-# for element in in_attributes:
-#     #
-#     factors_table.get_string()
-#     #factors_table.add_row(element, in_domains[element.split('.')])
+
+
 
 
